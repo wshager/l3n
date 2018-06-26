62ca93f46bc67aaa9545872840cdf9fb91e961f2
W.S. Hager <wshager@gmail.com>
2018-06-26 19:48:07 +0200

    minor fixes

--- src/construct.js
+++ src/construct.js
@@ -1,8 +1,12 @@
-import { isObservable, of } from "rxjs";
+import { isObservable, of, from } from "rxjs";
+
+import { map, concatMap, reduce } from "rxjs/operators";
 
 import { isVNode } from "./vnode";
 
 import { isQName } from "./qname";
+
+const just = $o => isObservable($o) ? $o : of($o);
 
 // faux VNode
 function vnode(inode, type, name, value) {
@@ -16,11 +20,15 @@ function vnode(inode, type, name, value) {
 }
 
 function _n(type, name, children) {
+	const len = children.length;
+	if(len == 1) {
+		children = children[0];
+	}
 	if (typeof name == "function") name = name();
 	if (typeof children == "function") children = children();
-	children = of(children).concatMap(function (c) {
-		return isObservable(c) ? c : isVNode(c) ? of(c) : x(c);
-	});
+	// convert to Observable
+	if(Array.isArray(children)) children = from(children);
+	children = just(children).pipe(concatMap(c => isObservable(c) ? c : just(isVNode(c) ? c : x(c))));
 	return vnode(function (parent, ref) {
 		var ns;
 		if (type == 1) {
@@ -33,11 +41,9 @@ function _n(type, name, children) {
 		}
 		// convert to real VNode instance
 		var node = parent.vnode(parent.emptyINode(type, name, type == 1 ? parent.emptyAttrMap() : undefined, ns), parent, parent.depth + 1, 0, type);
-		return children.concatMap(function (child) {
-			return child.inode(node);
-		}).reduce(function (node, child) {
-			return node.modify(child, ref);
-		}, node);
+		// create an inode from each child by calling the inode function on the faux VNode
+		// then wrap all nodes into the parent by calling modify
+		return children.pipe(concatMap(child => child.inode(node)),reduce((node, child) => node.modify(child, ref),node));
 	}, type, name);
 }
 
@@ -45,23 +51,23 @@ function _a(name, child) {
 	/*child = _seq.forEach((0, _seq.exactlyOne)(child),function (c) {
 		return (0, _seq.isSeq)(c) ? c : (0, _access.isVNode)(c) ? (c) : x(c);
 	});*/
-	return vnode(function (parent) {
+	child = just(child).pipe(concatMap(c => isObservable(c) ? c : just(isVNode(c) ? c : x(c))));
+	return vnode(parent => {
+		// provide a provisional entry in the parent (without a value)
 		var node = parent.vnode(parent.pair(name), parent);
 		// node is an attr node /w child as $val
-		return child.concatMap(function (child) {
-			return child.inode(node);
-		}).reduce(function (node, child) {
-			return node.modify(child, name);
-		}, node);
+		return child.pipe(concatMap(child => child.inode(node)),reduce((node, child) => node.modify(child),node));
 	}, 2, name);
 }
 
 function _v(type, val) {
-	return vnode(function (parent) {
-		return (val).map(function (val) {
-			return parent.vnode(parent.value(type, val), parent);
-		});
-	}, type, null, val);
+	return vnode(
+		// set by type, not by key (for attrs the keys are already filled in by pair)
+		parent => (isObservable(val) ? val : of(val)).pipe(map(val => parent.vnode(parent.set(null, val), parent))),
+		type,
+		null,
+		val
+	);
 }
 
 /**
@@ -71,15 +77,15 @@ function _v(type, val) {
  * @param  {[type]} children [description]
  * @return {[type]}          [description]
  */
-export function e(name, children) {
+export function e(name, ...children) {
 	return _n(1, name, children);
 }
 
-export function l(children) {
+export function l(...children) {
 	return _n(5, null, children);
 }
 
-export function m(children) {
+export function m(...children) {
 	return _n(6, null, children);
 }
 
