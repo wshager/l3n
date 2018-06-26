d5c17abaf69e7219801234a71b9dfbc7f523d3e6
W.S. Hager <wshager@gmail.com>
2018-06-26 19:49:02 +0200

    build

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import {} from "array-last-item";
 
+export { e,a,x,l,m } from "./construct";
+
 export { toVNodeStream, isLeaf, isBranch, isClose, isDirect } from "./l3n";
+
+export { vdoc } from "./access";
