6c678d7b06975f37c3810e56fb18fdcae101873d
W.S. Hager <wshager@gmail.com>
2018-07-10 14:27:33 +0200

    towards complete spec

--- src/vnode.js
+++ src/vnode.js
@@ -1,7 +1,25 @@
+/**
+ * core interface to communicate with nodes
+ * @type VNode
+ */
 export class VNode {
-	constructor(cx, inode, type, name, key, value, parent, depth, indexInParent, indexInCall, cache) {
+	/**
+	 * Create VNode
+	 * @param  {Object} cx            [description]
+	 * @param  {Object} node          [description]
+	 * @param  {Number} type          [description]
+	 * @param  {String} name          [description]
+	 * @param  {String} key           [description]
+	 * @param  {any} value         [description]
+	 * @param  {VNode} parent        [description]
+	 * @param  {Number} depth         [description]
+	 * @param  {Number} indexInParent [description]
+	 * @param  {Object} cache         [description]
+	 * @return {void}               [description]
+	 */
+	constructor(cx, node, type, name, key, value, parent, depth, indexInParent, cache) {
 		this.cx = cx;
-		this.inode = inode;
+		this.node = node;
 		this.type = type;
 		this.name = name;
 		this.key = key;
@@ -9,99 +27,91 @@ export class VNode {
 		this.parent = parent;
 		this.depth = depth | 0;
 		this.indexInParent = indexInParent;
-		this.indexInCall = indexInCall;
 		this.cache = cache;
 		this.__is_VNode = true;
 	}
+	/**
+	 * Render XML representation of a VNode
+	 * @param  {Function} [prettifier=x => x] Optional prettifier function
+	 * @return {String}               Output
+	 */
 	toString(prettifier = x => x) {
-		return this.cx.stringify(this.inode,this.type,prettifier);
+		return this.cx.stringify(this.node,this.type,prettifier);
 	}
 	toJS() {
-		return this.cx.toJS(this.inode,this.type);
+		return this.cx.toJS(this.node,this.type);
 	}
 	count() {
-		if (typeof this.inode == "function") return 0;
-		if (!this.cache) this.cache = this.cx.cached(this.inode, this.type);
-		return this.cx.count(this.inode, this.type, this.cache);
-	}
-	callCount() {
-		if (typeof this.inode == "function") return 0;
-		if (!this.cache) this.cache = this.cx.cached(this.inode, this.type);
-		return this.cx.callCount(this.inode, this.type, this.cache);
+		if (typeof this.node == "function") return 0;
+		if (!this.cache) this.cache = this.cx.cached(this.node, this.type);
+		return this.cx.count(this.node, this.type, this.cache);
 	}
 	keys() {
-		if (!this.cache) this.cache = this.cx.cached(this.inode, this.type);
-		return this.cx.keys(this.inode, this.type, this.cache);
+		if (!this.cache) this.cache = this.cx.cached(this.node, this.type);
+		return this.cx.keys(this.node, this.type, this.cache);
 	}
 	values() {
-		if (!this.cache) this.cache = this.cx.cached(this.inode, this.type);
-		return this.cx.values(this.inode, this.type, this.cache);
+		if (!this.cache) this.cache = this.cx.cached(this.node, this.type);
+		return this.cx.values(this.node, this.type, this.cache);
 	}
 	first() {
-		if (!this.cache) this.cache = this.cx.cached(this.inode, this.type);
-		return this.cx.first(this.inode, this.type, this.cache);
+		if (!this.cache) this.cache = this.cx.cached(this.node, this.type);
+		return this.cx.first(this.node, this.type, this.cache);
 	}
 	last() {
-		if (!this.cache) this.cache = this.cx.cached(this.inode, this.type);
-		return this.cx.last(this.inode, this.type, this.cache);
+		if (!this.cache) this.cache = this.cx.cached(this.node, this.type);
+		return this.cx.last(this.node, this.type, this.cache);
 	}
-	next(node) {
-		if (!this.cache) this.cache = this.cx.cached(this.inode, this.type);
-		return this.cx.next(this.inode, node.indexInParent, this.type, this.cache);
+	next(vnode) {
+		if (!this.cache) this.cache = this.cx.cached(this.node, this.type);
+		return this.cx.next(this.node, vnode, this.type, this.cache);
 	}
-	previous(node) {
-		if (!this.cache) this.cache = this.cx.cached(this.inode, this.type);
-		return this.cx.previous(this.inode, node.indexInParent, this.type, this.cache);
+	previous(vnode) {
+		if (!this.cache) this.cache = this.cx.cached(this.node, this.type);
+		return this.cx.previous(this.node, vnode, this.type, this.cache);
 	}
 	// TODO cache invalidation
 	push(kv) {
-		this.inode = this.cx.push(this.inode, kv, this.type, this.__hasCall);
+		this.node = this.cx.push(this.node, kv, this.type);
 		return this;
 	}
 	set(key, val) {
-		this.inode = this.cx.set(this.inode, key, val, this.type);
+		this.node = this.cx.set(this.node, key, val, this.type);
 		return this;
 	}
 	get(key) {
-		return this.cx.get(this.inode, key, this.type);
+		return this.cx.get(this.node, key, this.type);
 	}
 	has(key) {
-		return this.cx.has(this.inode, key, this.type);
+		return this.cx.has(this.node, key, this.type);
 	}
 	removeChild(child) {
-		this.inode = this.cx.removeChild(this.inode, child, this.type);
+		this.node = this.cx.removeChild(this.node, child, this.type);
 		return this;
 	}
 	finalize() {
-		this.inode = this.cx.finalize(this.inode);
+		this.node = this.cx.finalize(this.node);
 		return this;
 	}
 	attrEntries() {
-		return this.cx.attrEntries(this.inode);
+		return this.cx.attrEntries(this.node);
 	}
 	modify(node, ref) {
-		this.inode = this.cx.modify(this.inode, node, ref, this.type);
+		this.node = this.cx.modify(this.node, node, ref, this.type);
 		return this;
 	}
 	// hitch this on VNode for reuse
-	vnode(inode, parent, depth, indexInParent) {
-		return this.cx.vnode(inode, parent, depth, indexInParent);
+	vnode(node, parent, depth, indexInParent) {
+		return this.cx.vnode(node, parent, depth, indexInParent);
 	}
-	emptyINode(type, name, attrs, ns) {
-		return this.cx.emptyINode(type, name, attrs, ns);
-	}
-	emptyAttrMap(init) {
-		return this.cx.emptyAttrMap(init);
-	}
-	pair(key, inode) {
-		return this.cx.pair(key, inode);
+	create(type, name, attrs) {
+		return this.cx.create(type, name, attrs);
 	}
 }
 
-export class Step {
+export class Close {
 	constructor(node) {
 		this.node = node;
-		this.inode = node.inode;
 		this.parent = node.parent;
 		this.depth = node.depth;
 		this.indexInParent = node.indexInParent;
@@ -109,21 +119,10 @@ export class Step {
 		this.__is_VNode = true;
 	}
 	toString() {
-		return "Step {depth:" + this.node.depth + ", closes:" + this.node.name + "}";
-	}
-}
-
-export class Direct {
-	constructor(node) {
-		this.node = node;
-		node.__hasCall = node.count();
-		this.depth = node.depth;
-		this.type = 18;
-		this.__is_VNode = true;
-	}
-	toString(){
-		return `Direct {depth:${this.node.depth}, calls:${this.node.name}}`;
+		return `Close {depth: ${this.node.depth}, closes: ${this.node}}`;
 	}
 }
 
 export const isVNode = n => !!n && n.__is_VNode;
+
+export const getContext = (_this,defaultCx) => _this && _this.__vnode_context ? _this : defaultCx;
