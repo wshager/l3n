234c9c14abefaa5f6db699dc46c5b8123a936cb2
W.S. Hager <wshager@gmail.com>
2018-07-10 14:28:00 +0200

    towards complete spec

--- src/qname.js
+++ src/qname.js
@@ -1,13 +1,20 @@
-export function isQName(maybe) {
-	return !!(maybe && maybe.__is_QName);
+export function isQName(obj) {
+	return !!obj && obj instanceof QName;
 }
 
-export class QName {
+class QName {
 	constructor(uri, name) {
 		var prefix = /:/.test(name) ? name.replace(/:.+$/, "") : null;
 		this.name = name;
 		this.prefix = prefix;
 		this.uri = uri;
-		this.__is_QName = true;
 	}
+	toString() {
+		// TODO check scope and add ns attr
+		return (this.prefix ? this.prefix + ":" : "") + this.name;
+	}
+}
+
+export function qname(uri,name) {
+	return new QName(uri,name);
 }
