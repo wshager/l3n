e0e7f6e65aced2c4b36043db779b14619fc9628b
W.S. Hager <wshager@gmail.com>
2018-07-10 19:09:49 +0200

    updated tests

--- test/construct.js
+++ test/construct.js
@@ -1,3 +1,13 @@
 const l3 = require("../lib/index");
-const pnode = require("../lib/pnode");
-l3.t.bind(pnode)(l3.m(l3.a("bla","x"))).subscribe(x => console.log("x",x+""));
+
+// direct construction of a document-fragment, no Rx:
+
+const frag =
+	l3.e("div",
+		l3.a("class","greeting"),
+		l3.e("p","Hello")
+	).node(l3.inode);
+
+console.log(frag.toString());
+
+console.log(frag.toJS());
