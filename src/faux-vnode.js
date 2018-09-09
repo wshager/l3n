35108156d80698c64d2ab55bafca0aa35441b3e4
W.S. Hager <wshager@gmail.com>
2018-09-09 15:05:16 +0200

    add streaming flag for async children

--- src/faux-vnode.js
+++ src/faux-vnode.js
@@ -1,10 +1,11 @@
 // faux VNode
-export function vnode(node, type, name, value) {
+export function vnode(node, type, name, value,streaming = false) {
 	return {
 		node: node,
 		type: type,
 		name: name,
 		value: value,
+		streaming:streaming,
 		__is_VNode: true
 	};
 }
